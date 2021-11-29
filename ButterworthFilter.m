f7014b5f5f042e2c5b112c0d5bb0a1a2cce2fe8c
Amalmosalam <am.amaa9176@gmail.com>
2021-11-29 16:39:40 +0200

    added butterwoeth filter and gaussian filter

---
+++ ButterworthFilter.m
@@ -0,0 +1,45 @@
+function [newImage] = ButterworthFilter(image,D0,option)
+
+[h w l]=size(image);
+filter=zeros(h, w, l);
+
+if option==1
+    for i=1:h
+        for j=1:w
+        dis=sqrt((i-(h/2)).^2+(j-(w/2)).^2);
+            if dis<=D0
+                filter(i,j)=1/(1+(dis/D0)^2);    %lowpass
+            end
+        end
+    end
+else
+    for i=1:h
+        for j=1:w
+        dis=sqrt((i-(h/2)).^2+(j-(w/2)).^2);
+            if dis<=D0
+                filter(i,j)=1/(1+(D0/dis)^2);    %heighpass
+            end
+        end
+    end
+end
+
+    
+%     newImage=DomainConversions(image,1);
+%     newImage=newImage.*filter;
+%     newImage=DomainConversions(newImage,2);
+    
+    fourier=fft2(image);
+    fourier=fftshift(fourier);
+    reall=real(fourier);
+    imagine=imag(fourier);
+    reall=filter.*reall;
+    imagine=filter.*imagine;
+    newImage=reall+i*imagine;
+    newImage=fftshift(newImage); 
+    newImage=ifft2(newImage);
+    newImage=mat2gray((abs(newImage)));
+    
+end
+
+
+
